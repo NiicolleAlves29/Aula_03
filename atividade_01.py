bf9e82d887ccbcb9ac01a0fb4d9826baa432feab
NICOLLE ALVES FERREIRA <nicolle.ferreira@aluno.senac.rio>
2025-09-05 11:50:57 -0300

    Aula 3 - Estrutura if else elif

---
+++ atividade_01.py
@@ -0,0 +1,8 @@
+número = int(input('Digite o número:'))
+
+if número < 0:
+    print('Negativo!')
+elif número  > 0:
+    print('Positivo!')   
+else:
+    print('Zero é neutro!')    
---
+++ exemplo_01.py
@@ -0,0 +1,6 @@
+idade = int(input("Informe a sua idade:"))
+
+if idade >= 18:
+    print('Voce é maior!')
+else:
+    print('voce é menor de idade!')    
